54cb7e965d43aa0b83bb9f83e1ccdcca5d9e7a16
bokuzminski <bogdankuzminski@gmail.com>
2022-10-04 15:42:34 +0200

    feat: example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,27 +1,63 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'mad-wheel-picker';
+import { PickerIOS } from '@react-native-picker/picker';
+import MadWheelPicker from 'mad-wheel-picker';
+import { Platform, StyleSheet, View } from 'react-native';
+
+type Foo = {
+  selectedItem: number;
+  itemList: { label: string; value: number }[];
+};
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
+  const arr = [...Array(300).keys()].map((v, i) => ({
+    label: v.toString(),
+    value: i,
+  }));
+  const data: Foo = {
+    selectedItem: 2,
+    itemList: arr,
+  };
+  const [state, setState] = useState(data);
+  if (Platform.OS === 'ios') {
+    return (
+      <View style={styles.container}>
+        <PickerIOS
+          selectedValue={state.selectedItem}
+          onValueChange={(v) => setState((s) => ({ ...s, selectedItem: v }))}
+        >
+          {state.itemList.map(({ label, value }) => (
+            <PickerIOS.Item value={value} label={label} key={value} />
+          ))}
+        </PickerIOS>
+      </View>
+    );
+  } else if (Platform.OS === 'android') {
+    return (
+      <View style={styles.container}>
+        <MadWheelPicker
+          data={state.itemList}
+          onValueChange={onValueChange}
+          textSize={26}
+          textColor={'black'}
+          selectedTextColor={'red'}
+          selectedValue={state.selectedItem}
+          itemSpace={22}
+          style={{ height: 280, width: '100%' }}
+        />
+      </View>
+    );
+  } else {
+    return null;
+  }
+  function onValueChange(value: number) {
+    setState((s) => ({ ...s, selectedItem: value }));
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
     width: 60,
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,71 @@
-import { NativeModules, Platform } from 'react-native';
+import { PickerIOS } from '@react-native-picker/picker';
+import React from 'react';
+import {
+  NativeSyntheticEvent,
+  Platform,
+  requireNativeComponent,
+  ViewStyle,
+} from 'react-native';
 
-const LINKING_ERROR =
-  `The package 'mad-wheel-picker' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo managed workflow\n';
+export type MadWheelPickerItemProps = { label: string; value: number };
+type NativePickerProps = {
+  data: MadWheelPickerItemProps[];
+  selectedIndex: number;
+  onValueChange: (e: NativeSyntheticEvent<{ data: number }>) => void;
+  textColor: string;
+  selectedTextColor: string;
+  textSize: number;
+  itemSpace: number;
+  style: ViewStyle;
+};
 
-const MadWheelPicker = NativeModules.MadWheelPicker  ? NativeModules.MadWheelPicker  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+type MadWheelPicker = {
+  data: MadWheelPickerItemProps[];
+  selectedValue: number;
+  itemSpace: number;
+  textSize: number;
+  textColor: string;
+  selectedTextColor: string;
+  onValueChange: (v: number) => void;
+  style: ViewStyle;
+};
 
-export function multiply(a: number, b: number): Promise<number> {
-  return MadWheelPicker.multiply(a, b);
+const AndroidWheelPicker =
+  requireNativeComponent<NativePickerProps>('MadWheelPicker');
+
+class PickerItem extends React.Component<MadWheelPickerItemProps> {
+  render(): React.ReactNode {
+    return null;
+  }
 }
+
+class WheelCurvedPicker extends React.PureComponent<MadWheelPicker> {
+  constructor(props: MadWheelPicker) {
+    super(props);
+  }
+  static Item: typeof PickerItem = PickerItem;
+
+  private onValueChange = (e: NativeSyntheticEvent<{ data: number }>) => {
+    if (this.props.onValueChange) {
+      this.props.onValueChange(e.nativeEvent.data);
+    }
+  };
+
+  render() {
+    return (
+      <AndroidWheelPicker
+        {...this.props}
+        onValueChange={this.onValueChange}
+        data={this.props.data}
+        textSize={this.props.textSize}
+        selectedTextColor={this.props.selectedTextColor}
+        textColor={this.props.textColor}
+        itemSpace={this.props.itemSpace}
+        selectedIndex={this.props.selectedValue}
+      />
+    );
+  }
+}
+
+const MadWheelPicker = Platform.OS === 'ios' ? PickerIOS : WheelCurvedPicker;
+export default MadWheelPicker;
