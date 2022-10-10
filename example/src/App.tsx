2d5972af28211566c24a8c7fdb97341d057b099d
bokuzminski <bogdankuzminski@gmail.com>
2022-10-10 18:52:05 +0200

    added example project

---
+++ example/src/App.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+import type { ItemValue } from '@react-native-picker/picker/typings/Picker';
+import MadPicker from 'mad-wheel-picker';
+import { StyleSheet, View } from 'react-native';
+
+export default function App() {
+  const values = generatePickerItems();
+  const [state, setState] = useState(25);
+  console.log(state);
+
+  return (
+    <View style={styles.container}>
+      <MadPicker
+        selectedValue={state}
+        onValueChange={onValueChange}
+        style={styles.wheel}
+        textSize={32}
+        itemSpace={24}
+      >
+        {values.map(({ label, value }) => (
+          <MadPicker.Item label={label} value={value} key={label} />
+        ))}
+      </MadPicker>
+    </View>
+  );
+
+  function onValueChange(selectedValue: ItemValue) {
+    setState(selectedValue as number);
+  }
+}
+
+function generatePickerItems() {
+  return [...Array(100).keys()].map((num, index) => ({
+    label: num.toString(),
+    value: index,
+  }));
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  wheel: {
+    width: '100%',
+    height: 300,
+  },
+});
